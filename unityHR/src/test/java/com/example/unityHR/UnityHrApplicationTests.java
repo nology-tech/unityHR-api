534b81d450d4c65467374377ccb6cb5d1a4b66ab
Sam <samjoyce18@yahoo.com>
2021-12-03 11:48:47 +0000

    SpringBoot starter app added and made

---
+++ unityHR/src/test/java/com/example/unityHR/UnityHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unityHR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnityHrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
