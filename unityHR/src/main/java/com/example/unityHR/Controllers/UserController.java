4f0e4775b6134cc811eefd7cba4ef91b7ca11159
Barnali <babuxy@gmail.com>
2021-12-07 17:35:27 +0530

    Base Structure

--- unityHR/src/main/java/com/example/unityHR/Controllers/UserController.java
+++ unityHR/src/main/java/com/example/unityHR/Controllers/UserController.java
@@ -1,4 +1,38 @@
 package com.example.unityHR.Controllers;
+import com.example.unityHR.Models.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
 
 public class UserController {
+    private ArrayList<User> users = new ArrayList<>();
+
+    //Add user method
+    @PostMapping ("User/addUser")
+    public void addUser(@RequestBody User user ){
+        users.add(user);
+
+    }
+    //Fetch all users  method
+    @GetMapping ("User/getAllUsers")
+    public ArrayList<User> getUsers(){
+        return users;
+    }
+
+    //Update user method
+
+    //Delete single user method
+    @DeleteMapping("/User/{id}")
+    private void deleteEmployee(@RequestBody String id){
+        users.removeIf(user -> user.getId() == id);
+            users.remove(id);
+    }
+
+    //Bulk delete
+
+
+    }
+
 }
--- unityHR/src/main/java/com/example/unityHR/Models/User.java
+++ unityHR/src/main/java/com/example/unityHR/Models/User.java
@@ -1,4 +1,62 @@
 package com.example.unityHR.Models;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 
 public class User {
-}
+    private String id;
+    private String pronoun;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String emailVerified;
+    private String type;
+    private String jobTitle;
+    private long mobile;
+    private String department;
+    private String profileImage;
+    private String theme;
+    private String createdBy;
+    private String createdOn;
+    private String updatedBy;
+    private String updatedOn;
+
+    public User (String id, String pronoun, String firstName, String lastName, String address, String emailVerified,
+                 String type, String jobTitle, long mobile, String department, String profileImage, String theme) {
+        this.id = id;
+        this.pronoun = pronoun;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.emailVerified = emailVerified;
+        this.type = type;
+        this.jobTitle = jobTitle;
+        this.mobile = mobile;
+        this.department = department;
+        this.profileImage = profileImage;
+        this.theme = theme;
+    }
+
+        public String getId(){
+        return getId();
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public String getJobTitle() {
+            return jobTitle;
+        }
+
+        public String getDepartment() {
+            return department;
+        }
+
+
+    }
+
+
+
+
