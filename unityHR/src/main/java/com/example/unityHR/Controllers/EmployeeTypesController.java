cc8b4775d57c888674e5ff14e5c139a38c9df9d2
Shalaka Stephen <shalaka.stephen@db.com>
2021-12-08 10:55:40 +0530

    initial MVC for employee types

--- unityHR/src/main/java/com/example/unityHR/Controllers/EmployeeTypesController.java
+++ unityHR/src/main/java/com/example/unityHR/Controllers/EmployeeTypesController.java
@@ -1,4 +1,32 @@
 package com.example.unityHR.Controllers;
+import com.example.unityHR.Models.EmployeeTypes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
 
+@RestController
 public class EmployeeTypesController {
+
+    private ArrayList<EmployeeTypes> employeetypes = new ArrayList<>();
+//    EmployeeTypes standardtypes= new EmployeeTypes("001","standard-employee");
+//    EmployeeTypes hrtypes= new EmployeeTypes("002","hr-employee");
+//    EmployeeTypes admintypes= new EmployeeTypes("002","admin-employee");
+
+
+    @PostMapping("/employeetype")
+    public ResponseEntity<String> createEmployeeTypes(@RequestBody EmployeeTypes employeetype)  {
+            employeetypes.add(employeetype);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Employee types added");
+    }
+
+    @GetMapping("/employeetype")
+    public ResponseEntity<ArrayList<EmployeeTypes>> getEmployeeTypes(){
+        return ResponseEntity.status(HttpStatus.OK).body(employeetypes);
+    }
+
+
 }
--- unityHR/src/main/java/com/example/unityHR/Models/EmployeeTypes.java
+++ unityHR/src/main/java/com/example/unityHR/Models/EmployeeTypes.java
@@ -1,4 +1,31 @@
 package com.example.unityHR.Models;
 
 public class EmployeeTypes {
+    private String id;
+    private String name;
+
+    public EmployeeTypes(String id,String name){
+        this.id= id;
+        this.name= name;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "EmployeeTypes{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
+
+
+
