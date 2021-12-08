f488d578896e6cb0a849aec529429a3d14b69154
Sandhya Sharma <sandhya.dube@gmail.com>
2021-12-08 11:02:46 +0530

    initial setting done

--- unityHR/src/main/java/com/example/unityHR/Controllers/JobTitlesController.java
+++ unityHR/src/main/java/com/example/unityHR/Controllers/JobTitlesController.java
@@ -1,4 +1,57 @@
 package com.example.unityHR.Controllers;
+import com.example.unityHR.Models.JobTitles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.util.ArrayList;
+
+@RestController
 public class JobTitlesController {
+   private ArrayList<JobTitles> jobTitles = new ArrayList<>();
+
+    //@Autowired
+    //JobTitlesRepository repository;
+
+    @GetMapping("/job-titles")
+    public ResponseEntity<ArrayList<JobTitles>> getJobTitles() {
+        String jobTitlesSizeHeaderValue = "Count: " + jobTitles.size();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header("AllJobTitles",jobTitlesSizeHeaderValue)
+                .body(jobTitles);
+    }
+
+    @PostMapping("/job-title")
+    public ResponseEntity<String> createJobTitle(@RequestBody JobTitles jobTitle) {
+        //String jobTitlesSizeHeaderValue = "Count: " + jobTitles.size();
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Job Title Added", "1")
+                .body(jobTitle.getJobTitle());
+    }
+
+
+
+
+
+
 }
+
+//405 Method Not Allowed
+//408 Request Timeout
+//404 Not Found
+//        400 Bad Request
+//        The server could not understand the request due to invalid syntax.
+//401 Unauthorized
+//Although the HTTP standard specifies "unauthorized", semantically this response means "unauthenticated". That is, the client must authenticate itself to get the requested response.
+// return ResponseEntity.ok().body(greetings);
+//return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+
+
+
+
+// 410 Gone
+//414 URI Too Long
--- unityHR/src/main/java/com/example/unityHR/Models/JobTitles.java
+++ unityHR/src/main/java/com/example/unityHR/Models/JobTitles.java
@@ -1,4 +1,36 @@
 package com.example.unityHR.Models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class JobTitles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private String jobTitleId;
+    private String jobTitle;
+
+    public JobTitles(String jobTitle) {
+          this.jobTitle = jobTitle;
+    }
+
+    public String getJobTitleId() {
+        return jobTitleId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "JobTitles{" +
+                "jobTitleId='" + jobTitleId + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                '}';
+    }
 }
