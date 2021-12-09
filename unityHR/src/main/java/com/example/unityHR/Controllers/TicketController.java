17cbbe5b4afdaeab51e1c87a226ab64bd8aa21d6
RamyaG85 <ramya_gullapalli@yahoo.com>
2021-12-09 10:35:38 +0530

    For Ticket - Added repository, database connection, modified methods of endpoints and made 'id' to int

--- unityHR/src/main/java/com/example/unityHR/Controllers/TicketController.java
+++ unityHR/src/main/java/com/example/unityHR/Controllers/TicketController.java
@@ -8,32 +8,34 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.ArrayList;
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
+
+import static java.lang.Integer.parseInt;
 
 @RestController
 public class TicketController {
-        private ArrayList<Ticket> tickets = new ArrayList<>();
+//        private ArrayList<Ticket> tickets = new ArrayList<>();
 
         @Autowired
-        TicketRepository repository;
+        TicketRepository ticketRepository;
 
         //GET all tickets
         @GetMapping("/tickets")
         public ResponseEntity<List<Ticket>> getTickets(){
                 //  return ResponseEntity.status(HttpStatus.OK).body(tickets);
-            return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
-
+            return ResponseEntity.status(HttpStatus.OK).body(ticketRepository.findAll());
         }
 
         //GET a ticket by ID quote
         @GetMapping("/ticket/{id}")
-        public ResponseEntity<Ticket> getTicket(@PathVariable String id){
+        public ResponseEntity<Optional<Ticket>> getTicket(@PathVariable String id){
                 // This has to be changed to get by Id ...Currently, it access the id by using position
                 // Also, the validation check whether the id is valid or not to be added
                 // Currently, the Path variable 'id' is taken as an int, but the actual 'id' in ticket is 'String'
                 // return ResponseEntity.status(HttpStatus.OK).body(tickets.get(id));
-            return ResponseEntity.status(HttpStatus.OK).body(repository.getById(id));
+            return ResponseEntity.status(HttpStatus.OK).body(ticketRepository.findById(parseInt(id)));
 
         }
 
@@ -43,17 +45,18 @@ public class TicketController {
                 // Mandatory field validations need to be discussed once
                 //  tickets.add(ticket);
                 // ticket id can be shown in the response body - future
-                repository.save(ticket);
+                ticketRepository.save(ticket);
                 return ResponseEntity.status(HttpStatus.CREATED).body("Ticket has been added");
         }
 
         //Delete a ticket
         @DeleteMapping("/ticket/{id}")
+        @Transactional
         public ResponseEntity<String> deleteTicket(@PathVariable String id){
                 // Valid 'id' check has to be added
                 //  tickets.remove(index);
                 // tickets.removeIf(ticket -> ticket.getId() == id);
-            repository.deleteById(id);
+                ticketRepository.deleteById(parseInt(id));
             return ResponseEntity.status(HttpStatus.GONE).body("Deleted ticket : " + id);
         }
 
