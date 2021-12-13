7f3d0ee87ab7d55bcc48a1304ad8b2a1c5276db9
kirti vaishya <kirti.vaishya@gmail.com>
2021-12-13 15:45:46 +0530

    delete update

--- unityHR/src/main/java/com/example/unityHR/Controllers/UserController.java
+++ unityHR/src/main/java/com/example/unityHR/Controllers/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @RestController
+@CrossOrigin
 public class UserController {
     private ArrayList<User> users = new ArrayList<>();
 
