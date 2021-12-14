189701a89143830f2f1d567505e4fbd4b3eb3755
Prema <prema.chhaya@gmail.com>
2021-12-14 17:46:27 +0530

    patch method implemented for user

--- unityHR/src/main/java/com/example/unityHR/Controllers/UserController.java
+++ unityHR/src/main/java/com/example/unityHR/Controllers/UserController.java
@@ -2,17 +2,27 @@ package com.example.unityHR.Controllers;
 import com.example.unityHR.Models.User;
 //import com.example.unityHR.Repositories.UserRepository;
 import com.example.unityHR.Repositories.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     private ArrayList<User> users = new ArrayList<>();
+    Logger logger = LoggerFactory.getLogger(UserController.class);
 
   @Autowired(required=false)
   UserRepository userRepo;
@@ -50,6 +60,82 @@ public class UserController {
     private ResponseEntity<String> deleteEmployees(@RequestBody ArrayList<String> userlist){
        userRepo.deleteAllById(userlist);
        return ResponseEntity.status(HttpStatus.GONE).body("Bulk delete successfull");
+    }
+
+//    private User applyPatchToCustomer(
+//            JsonPatch patch, User targetUser) throws JsonPatchException, JsonProcessingException {
+//        JsonNode patched = patch.apply(objectMapper.convertValue(targetUser, JsonNode.class));
+//        return objectMapper.treeToValue(patched, Customer.class);
+//    }
+
+    //Patch User
+//
+//    @PatchMapping("/User/{emailVerified}")
+//    @Transactional
+//    public ResponseEntity<User> updateEmployees(@PathVariable String emailVerified, @RequestBody User user){
+//
+//        logger.error(emailVerified);
+//        logger.error(emailVerified,user);
+//
+//        User existingUser = userRepo.getByEmailVerified(emailVerified);
+//
+//        logger.error(existingUser.getUUIDUser().toString());
+//
+//     if (existingUser != null) {
+//          userRepo.updateMobileByFirebaseId(emailVerified, user.getFirstName(),user.getLastName(),user.getPronoun(), user.getAddress(), user.getMobileNo());
+//        }
+//
+//        User updatedUser = userRepo.getByEmailVerified(emailVerified);
+//
+//       // return ResponseEntity.status(HttpStatus.OK).body("user updated");
+//
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(updatedUser);
+//    }
+
+    @PatchMapping("/User/{emailVerified}/{updatedFieldName}")
+    @Transactional
+    public ResponseEntity<User> updateEmployeeDetail(@PathVariable String emailVerified, @PathVariable String updatedFieldName, @RequestBody String updatedFieldValue){
+
+        logger.error(emailVerified);
+        logger.error(updatedFieldName);
+        logger.error(updatedFieldValue);
+
+        User existingUser = userRepo.getByEmailVerified(emailVerified);
+
+        logger.error(existingUser.getUUIDUser().toString());
+
+        if (existingUser != null) {
+
+            switch (updatedFieldName) {
+                case "firstName":
+                    userRepo.updateMobileByFirebaseId(emailVerified, updatedFieldValue,existingUser.getLastName(),existingUser.getPronoun(), existingUser.getAddress(), existingUser.getMobileNo());
+                    break;
+                case "lastName":
+                    userRepo.updateMobileByFirebaseId(emailVerified, existingUser.getFirstName(),updatedFieldValue,existingUser.getPronoun(), existingUser.getAddress(), existingUser.getMobileNo());
+                    break;
+                case "pronoun":
+                    userRepo.updateMobileByFirebaseId(emailVerified, existingUser.getFirstName(),existingUser.getLastName(), updatedFieldValue, existingUser.getAddress(), existingUser.getMobileNo());
+                    break;
+                case "address":
+                    userRepo.updateMobileByFirebaseId(emailVerified, existingUser.getFirstName(), existingUser.getLastName(), existingUser.getPronoun(), updatedFieldValue, existingUser.getMobileNo());
+                    break;
+                case "mobileNo":
+                    userRepo.updateMobileByFirebaseId(emailVerified, existingUser.getFirstName(), existingUser.getLastName(),existingUser.getPronoun(), existingUser.getAddress(), updatedFieldValue);
+                    break;
+                default:
+                    break;
+            }
+            //existingUser.setMobileNo(updatedFieldValue);
+
+        }
+
+        User updatedUser = userRepo.getByEmailVerified(emailVerified);
+
+        // return ResponseEntity.status(HttpStatus.OK).body("user updated");
+
+
+        return ResponseEntity.status(HttpStatus.OK).body(updatedUser);
     }
 
 
--- unityHR/src/main/java/com/example/unityHR/Repositories/UserRepository.java
+++ unityHR/src/main/java/com/example/unityHR/Repositories/UserRepository.java
@@ -2,9 +2,25 @@ package com.example.unityHR.Repositories;
 
 import com.example.unityHR.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,String> {
    User getByFirebaseId(String firebaseId);
+   User getByEmailVerified(String emailVerified);
+
+
+   @Modifying(clearAutomatically = true)
+   @Query("UPDATE User SET  first_name = :firstName, last_name = :lastName, pronoun = :pronoun, address = :address, mobile_no = :mobileNo WHERE email_verified = :emailVerified")
+
+   void updateMobileByFirebaseId(@Param("emailVerified") String emailVerified, @Param("firstName") String firstName,
+                                 @Param("lastName") String lastName,
+                                 @Param("pronoun") String pronoun,
+                                 @Param("address") String address,
+                                 @Param("mobileNo") String mobileNo );
 }
