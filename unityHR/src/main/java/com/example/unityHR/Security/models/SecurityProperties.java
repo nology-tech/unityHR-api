29635cf052bc0178ba6e8dee3d321578e34340cb
Yujin <yujinwunz@gmail.com>
2021-12-22 18:23:01 +1100

    Spring boot security with firebase integration

---
+++ unityHR/src/main/java/com/example/unityHR/Security/models/SecurityProperties.java
@@ -0,0 +1,22 @@
+package com.example.unityHR.Security.models;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@ConfigurationProperties("security")
+@Data
+public class SecurityProperties {
+
+    FirebaseProperties firebaseProps;
+    boolean allowCredentials;
+    List<String> allowedOrigins;
+    List<String> allowedHeaders;
+    List<String> exposedHeaders;
+    List<String> allowedMethods;
+    List<String> allowedPublicApis;
+
+}
